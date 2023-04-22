8cba73439b9f7737c2aa9a899e994393b1c46bcd
Panupong Rong-O <tayalone@gmail.com>
2023-04-22 11:11:07 +0700

    🔥 remove useless code

--- lib/session.ts
+++ lib/session.ts
@@ -1,6 +1,5 @@
 // this file is a wrapper with defaults to be used in both API routes and `getServerSideProps` functions
 import type { IronSessionOptions } from "iron-session";
-import type { User } from "pages/api/user";
 import type { BlogObject } from "pages/api/blogs";
 
 
@@ -16,7 +15,6 @@ export const sessionOptions: IronSessionOptions = {
 // This is where we specify the typings of req.session.*
 declare module "iron-session" {
   interface IronSessionData {
-    user?: User;
     uniqueID?: string;
     blogWatching?:  BlogObject,
   }
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,19 +1,17 @@
 import { AppProps } from 'next/app'
-import { SWRConfig } from 'swr'
-import fetchJson from 'lib/fetchJson'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <SWRConfig
-      value={{
-        fetcher: fetchJson,
-        onError: (err) => {
-          console.error(err)
-        },
-      }}
-    >
+    // <SWRConfig
+    //   value={{
+    //     fetcher: fetchJson,
+    //     onError: (err) => {
+    //       console.error(err)
+    //     },
+    //   }}
+    // >
       <Component {...pageProps} />
-    </SWRConfig>
+    // </SWRConfig>
   )
 }
 
